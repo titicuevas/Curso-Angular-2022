b7511336924a7b4f1b6579efa44e00af91a9ba14
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2022-10-12 22:28:01 +0200

    Primer programa
    Primeros ficheros , todavia queda por terminar.

---
+++ ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,14 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre :string = 'Henry';
+let hp :number |string =95;
+let estaVivo:boolean = true;
+
+
+
+hp = 'FULL';
+
+console.log(nombre, hp);
